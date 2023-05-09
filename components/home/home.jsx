7efb5ae8245a341bd0554b57fd5c90f7ac7e9a22
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2023-05-09 14:46:31 -0500

    avances

--- components/home/home.jsx
+++ components/home/home.jsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { Text, View } from "react-native";
 import CharacterCard from "../characterCard/CharacterCard";
+import apiParams from "../../config";
+import axios from "axios";
 
 const Home = () => {
   return (
     <View>
-      <Text>Home componente</Text>
       <CharacterCard name="Hulk" image={require("../img/Hulk.png")} />
       <CharacterCard name="IronMan" image={require("../img/IronMan.png")} />
     </View>
